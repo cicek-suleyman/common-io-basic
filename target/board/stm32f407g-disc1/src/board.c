fff086d8c566ac33124d9efef1b6dcf2e1dba0c0
Süleyman ÇİÇEK <suleyman@cicek.pw>
2026-02-15 02:09:00 +0300

    nvic exception handling issue fix
    SVC_Handler hard fault fixed

--- target/board/stm32f407g-disc1/src/board.c
+++ target/board/stm32f407g-disc1/src/board.c
@@ -113,7 +113,6 @@ void HAL_MspInit(void) {
         __HAL_RCC_SYSCFG_CLK_ENABLE();
         __HAL_RCC_PWR_CLK_ENABLE();
 
-        HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
         SystemClock_Config();
 }
 
